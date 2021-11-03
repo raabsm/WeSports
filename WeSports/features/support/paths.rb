f21af5fa3690bc6824f294a64c7fb32a6f39ceb0
Sam Raab <raabsm@gmail.com>
2021-11-03 15:55:08 -0400

    added new game test

--- WeSports/features/support/paths.rb
+++ WeSports/features/support/paths.rb
@@ -13,8 +13,8 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-    when /^the movies page$/ then '/movies'
+    when /^the (WeSports )?home\s?page$/ then '/games'
+    when /^the new game page$/ then '/games/new'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
