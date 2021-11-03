5fe61a297689e7582e5d80e1d2b7a3c5d5b829fa
Sam Raab <raabsm@gmail.com>
2021-11-03 01:30:11 -0400

    edited spots_left bug

--- WeSports/app/models/game.rb
+++ WeSports/app/models/game.rb
@@ -1,2 +1,11 @@
 class Game < ActiveRecord::Base
+    def spots_left
+        if slots_to_be_filled.nil?
+            return 0
+        elsif slots_taken.nil?
+            return slots_to_be_filled
+        else
+            return slots_to_be_filled - slots_taken
+        end
+    end
 end
