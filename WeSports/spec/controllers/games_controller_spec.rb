7264baa5ebbcd06e88c74f2b624b1d156b7688ba
Stephane Meunier <Stephbysteph16@gmail.com>
2021-11-04 02:11:24 -0400

    Added Rspec files before testing in VM

--- WeSports/spec/controllers/games_controller_spec.rb
+++ WeSports/spec/controllers/games_controller_spec.rb
@@ -30,15 +30,124 @@ RSpec.describe GamesController, type: :controller do
       Game.create(:sport_name => "soccer",
                   :zipcode => "10010",
                   :slots_to_be_filled => 10,
-                  :slots_taken => 7)
+                  :slots_taken => 10)
     end
   end
 
   describe "When trying to filter by name with a valid sport name" do
     it "returns a valid list of games filtered by that sport name" do
-      
+      get :index, {:name_search => "Basketball"}
+
+      expect(assigns(:games).length).not_to eq(0)
+
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Basketball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"basketball"))
+
+      expect(assigns(:games)).not_to include(Game.find_by(:sport_name=>"Spikeball"))
+      expect(assigns(:games)).not_to include(Game.find_by(:sport_name=>"Football"))
+
     end
 
+  end
+
+  describe "When trying to filter by name with an empty sport name" do
+    it "returns the list of all games" do
+      get :index, {:name_search => ""}
+
+      expect(assigns(:games).length).to eq(5)
+
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Basketball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"basketball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Spikeball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Football"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"soccer"))
+
+    end
+
+  end
+
+  describe "When trying to filter by name with an invalid sport name" do
+    it "returns an empty list of games" do
+      get :index, {:name_search => "invalid"}
+
+      expect(assigns(:games).length).to eq(0)
+    end
+  end
+
+  describe "When the only available button is not checked" do
+    it "Shows the entire list of games" do
+      get :index, {:only_available => 0}
+
+      expect(assigns(:games).length).to eq(5)
+
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Basketball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"basketball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Spikeball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Football"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"soccer"))
+
+    end
+  end
+
+  describe "When the only available button is checked" do
+    it "Shows the entire list of games" do
+      get :index, {:only_available => 1}
+
+      expect(assigns(:games).length).to eq(4)
+
+      expect(assigns(:games)).not_to include(Game.find_by(:sport_name=>"soccer"))
+
+    end
+  end
+
+
+
+  describe "When trying to filter by zipcode" do
+    it "returns a valid list of games filtered by that zipcode" do
+      get :index,{:zip_search => "10010"}
+      expect(assigns(:games).length).to eq(1)
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"soccer"))
+    end
+  end
+
+  describe "When trying to filter by blank zipcode" do
+    it "returns a valid list of games filtered by that zipcode" do
+      get :index,{:zip_search => ""}
+      expect(assigns(:games).length).to eq(5)
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Basketball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"basketball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Spikeball"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Football"))
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"soccer"))
+    end
+  end
+
+  describe "When trying to filter by invalid zipcode" do
+    it "returns a valid list of games filtered by that zipcode" do
+      get :index,{:zip_search => "abc"}
+      expect(assigns(:games).length).to eq(0)
+      expect(assigns(:games)).not to include(Game.find_by(:sport_name=>"soccer"))
+    end
+  end
+
+  describe "When trying to filter by zip code and sport name" do
+    it "returns a list of games with sport name equal to the sport name search and with same zip code" do
+      get :index, {:name_search => "Basketball", :zip_search => 10025}
+      expect(assigns(:games).length).to eq(1)
+
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"basketball"))
+
+      expect(assigns(:games)).not_to include(Game.find_by(:sport_name=>"basketball"))
+
+    end
+  end
+
+  describe "When trying to filter by sport name, zipcode and only available" do
+    it "returns a valid list of games filtered by that zipcode" do
+      get :index,{:zip_search => "10024", :only_available => 0, :name_search=>"Basketball"}
+      expect(assigns(:games).length).to eq(1)
+      expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Basketball"))
+    end
   end
 
 end
