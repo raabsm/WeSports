109b170abef719d3cfc805719e55df55badc5cb7
Sam Raab <raabsm@gmail.com>
2021-11-04 04:14:23 -0400

    added new game and update game tests

--- WeSports/spec/controllers/games_controller_spec.rb
+++ WeSports/spec/controllers/games_controller_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe GamesController, type: :controller do
 
   describe "When trying to filter by name with a valid sport name" do
     it "returns a valid list of games filtered by that sport name" do
-      get :index, {:name_search => "Basketball"}
+      get :index, :params => {'name_search' => "Basketball"}
 
       expect(assigns(:games).length).not_to eq(0)
 
@@ -147,6 +147,83 @@ RSpec.describe GamesController, type: :controller do
       get :index,{:zip_search => "10024", :only_available => 0, :name_search=>"Basketball"}
       expect(assigns(:games).length).to eq(1)
       expect(assigns(:games)).to include(Game.find_by(:sport_name=>"Basketball"))
+    end
+  end
+
+  describe "Add New Game" do
+    it "with valid parameters" do
+      previous_len = Game.all().count
+      get :create, {:game => {:sport_name => "Wiffle Ball",
+                                        :zipcode => "10027",
+                                        :slots_to_be_filled => 20,
+                                        :slots_taken => 10}}
+      expect(response).to redirect_to games_path
+      expect(flash[:notice]).to match(/Successfully created Wiffle Ball game/)
+      expect(Game.all().count).to eq(previous_len + 1)
+      Game.find_by(:sport_name => "Wiffle Ball").destroy
+    end
+
+    it "Game with no name or zipcode parameters" do
+      get :create, {:game => {:sport_name => "",
+                                        :zipcode => "10027",
+                                        :slots_to_be_filled => 20
+                                        }}
+      expect(response).to redirect_to new_game_path
+      expect(flash[:notice]).to match(/Error: Missing Zip Code or Sport Name Fields/)
+    end
+
+    it "Game with no available slots" do
+      get :create, {:game => {:sport_name => "Soccer",
+                                        :zipcode => "10027",
+                                        :slots_to_be_filled => ""
+                                        }}
+      expect(response).to redirect_to new_game_path
+      expect(flash[:notice]).to match(/Error: Total Slots Available not valid/)
+    end
+  end
+
+  describe "Update existing game" do
+    it "Successfully edits the name" do
+      game = Game.create!(:sport_name => "Football",
+                         :zipcode => "10026",
+                         :slots_to_be_filled => 20,
+                         :slots_taken => 5)
+
+      get :update, {:id => game.id, :game =>
+        {:sport_name => "New Sport", :zipcode => "10026",
+         :slots_to_be_filled => 20, :slots_taken => 5} }
+
+      expect(response).to redirect_to game_path(game)
+      expect(flash[:notice]).to match(/Successfully updated game/)
+      expect(Game.find_by(:sport_name => "New Sport").length).to eq(1)
+    end
+
+    it "Doesn't include zipcode or name" do
+      game = Game.create(:sport_name => "New Sport",
+                         :zipcode => "10026",
+                         :slots_to_be_filled => 20,
+                         :slots_taken => 5)
+      get :update, {:id => game.id, :game =>
+        {:sport_name => "", :zipcode => "10026",
+         :slots_to_be_filled => 20} }
+
+      expect(response).to redirect_to edit_game_path(game)
+      expect(flash[:notice]).to match(/Error: Missing Zip Code or Sport Name Fields/)
+      expect(Game.find_by(:sport_name => "New Sport").length).to eq(1)
+    end
+
+    it "Fewer Total Slots than slots taken" do
+      game = Game.create(:sport_name => "New Sport",
+                         :zipcode => "10026",
+                         :slots_to_be_filled => 20,
+                         :slots_taken => 5)
+      put :update, {:id => game.id, :game =>
+        {:slots_to_be_filled => 4, :zipcode => "10026",
+         :sport_name => "New Sport"} }
+
+      expect(response).to redirect_to edit_game_path(game)
+      expect(flash[:notice]).to match("Error: More Slots Taken (5) than Available")
+      expect(Game.find_by(:sport_name => "New Sport").slots_taken).to eq(5)
     end
   end
 
