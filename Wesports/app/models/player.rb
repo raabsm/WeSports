96db29f9465fa678f2d315a9c200068022ba7c0b
Stephane Meunier <Stephbysteph16@gmail.com>
2021-12-04 00:49:47 -0500

    Improved UI design

---
+++ Wesports/app/models/player.rb
@@ -0,0 +1,23 @@
+class Player < ActiveRecord::Base
+  validates :username, :email, presence: true
+  validates :email, uniqueness: true
+  has_and_belongs_to_many :games, join_table: 'games_players'
+  
+  def self.create_from_omniauth(auth)
+        Player.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['first_name']
+            u.email = auth['info']['email']
+        end
+  end
+
+  def self.check_player_exist(player_id)
+    if exists?(id: player_id)
+      notice = ""
+      valid = true
+    else
+      notice = "Error: Player does not exist"
+      valid = false
+    end
+    return valid, notice
+  end
+end
