2e47de5ca30c7a0b0cd4d39e2cd6c3081986b97d
Sam Raab <raabsm@gmail.com>
2021-12-16 16:13:49 -0500

    added rspec tests and improved coverage

--- WeSports/app/models/player.rb
+++ WeSports/app/models/player.rb
@@ -14,14 +14,14 @@ class Player < ActiveRecord::Base
     return Game.where(:owning_player_id => id)
   end
 
-  def self.check_player_exist(player_id)
-    if exists?(id: player_id)
-      notice = ""
-      valid = true
-    else
-      notice = "Error: Player does not exist"
-      valid = false
-    end
-    return valid, notice
-  end
+  # def self.check_player_exist(player_id)
+  #   if exists?(id: player_id)
+  #     notice = ""
+  #     valid = true
+  #   else
+  #     notice = "Error: Player does not exist"
+  #     valid = false
+  #   end
+  #   return valid, notice
+  # end
 end
